60bacf2cab1a6562a3b7d779d87f8f0f7416c961
Harsh RB <harshrb2424@gmail.com>
2026-03-10 15:29:55 +0530

    Update .gitignore, enhance app.js for static images, modify schema.sql for user table, and improve friends.repository.js formatting

--- src/app.js
+++ src/app.js
@@ -8,13 +8,16 @@ import errorMiddleware from './shared/middleware/error.middleware.js';
 import { jwtChecker } from './shared/middleware/jwtChecker.js';
 import cors from 'cors';
 
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
+app.use('/images', express.static('images'));
 app.use(express.json());
 app.use(cors({
-    origin:  [
-    "http://localhost:5173",
-    "http://localhost:5174"
-  ],
+    origin: "*",
     credentials: true
 }));
 
